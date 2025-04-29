36b75f2e6bb2ab5e8a45b5bae508e954b0ea6a0f
Kyle Xiang <cxiang@lcs.on.ca>
2025-04-29 19:48:29 -0400

    Initial Commit

---
+++ BudgetBuddyApps/BudgetBuddyAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBuddyAppsApp.swift
+//  BudgetBuddyApps
+//
+//  Created by Chujun Xiang on 2025-04-29.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBuddyAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
